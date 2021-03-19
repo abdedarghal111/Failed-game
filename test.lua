9a9abde03ad5d9a9639732f48b9bfb8f9db0ef26
Abderrahim <abdedarghal111@gmail.com>
2021-03-19 16:23:30 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -3,9 +3,9 @@ local img = imga.new(_,_,_,_,asserts.img.somefloor)
 
 addcoroutine("f",function()
 while true do
-  img.rotation = img.rotation + 1
-  img.position.x = img.position.x + 1
-  wait()
-  img:draw()
-end
+    img.rotation = img.rotation + 1
+    img.position.x = img.position.x + 1
+    wait()
+    img:draw()
+  end
 end)
